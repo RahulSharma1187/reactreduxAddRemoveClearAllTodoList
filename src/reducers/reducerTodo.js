8bb00feff45811e517b3af3185c94e5ecc0e8886
Rahul Sharma <rahul.sharma@webchutney.net>
2021-05-20 14:05:16 +0530

    validation

--- src/reducers/reducerTodo.js
+++ src/reducers/reducerTodo.js
@@ -6,15 +6,17 @@ const todoList = (state = initialState, action) => {
     switch(action.type){
         case "ADDTODO" : 
             const {id, data} = action.payload;
-            return {
-                ...state,
-                list: [
-                    ...state.list,
-                    {
-                        id:id,
-                        data: data
-                    }
-                ]
+            if(data.length > 2){
+                return {
+                    ...state,
+                    list: [
+                        ...state.list,
+                        {
+                            id:id,
+                            data: data
+                        }
+                    ]
+                }
             }
             
         case "DELETETODO" : 
